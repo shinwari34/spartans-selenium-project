cfc709c20e3fd51b2e187ee0d3d409c7e672327a
Himmat <Najeebullah.shinwari34@gmail.com>
2024-07-21 16:09:55 -0600

    Add this commit

---
+++ src/test/java/locators/CSS/UsingCssSelector.java
@@ -0,0 +1,27 @@
+package locators.CSS;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsingCssSelector {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(10000);
+
+        By titleLocator = By.cssSelector("div.home__categories-grid > div:nth-child(2) > h1");
+        String text = driver.findElement(titleLocator).getText();
+
+        System.out.println(text);
+
+        driver.quit();
+    }
+
+    public By getButtonLocator(String buttonText) {
+        return By.xpath("//button[text() ='" + buttonText + "']");
+    }
+}
